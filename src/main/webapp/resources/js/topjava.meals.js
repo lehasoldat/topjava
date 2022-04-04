4148e11eeb8c9dba59d736921dda589d46c00e6c
lehasoldat <saldakeev@gmail.com>
2022-04-04 22:09:37 +0300

    HW08_01_Add MealUIController and DataTable in meals.jsp

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,30 @@
+const mealAjaxUrl = "profile/meals/";
+
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+}
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {"data": "dateTime"},
+                {"data": "description"},
+                {"data": "calories"},
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [0, "desc"]
+            ]
+        })
+    )
+})
